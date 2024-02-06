19a98994f6f4a3b34f7b80e7f225cda20ff89283
unknown <ilramm@yandex.ru>
2024-02-06 23:03:49 +0500

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,39 +2,64 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 )
 
-var urlID string
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-func handleGet(wr http.ResponseWriter, rr *http.Request) {
+var urlKeys map[string]string
 
-	originalGetURL := rr.URL.String()
-	urlID = originalGetURL[1:]
-	host := rr.Host
+func generateRandomString(length int) string {
 
-	wr.Header().Add("Location", "https://practicum.yandex.ru/")
-	wr.WriteHeader(http.StatusTemporaryRedirect)
-	fmt.Fprintf(wr, "%s\n%s", urlID, host)
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
 
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
 
-	contentType := r.Header.Get("Content-Type")
-	originalPostURL := r.Header.Get("URL")
+	theURL := r.Header.Get("URL")
+	host := r.Host
+	randomString := generateRandomString(5)
 
-	if contentType != "text/plain" {
-		http.Error(w, "Unsupported content type", http.StatusUnsupportedMediaType)
-		return
-	}
+	urlKeys = make(map[string]string, 10)
+	urlKeys[randomString] = theURL
 
-	fmt.Fprintf(w, "Received data: %s, %s", contentType, originalPostURL)
+	finalURL := host + "/" + randomString
+	w.Header().Set("URL", finalURL)
+	fmt.Fprintf(w, "%s", urlKeys[randomString])
 	w.WriteHeader(http.StatusCreated)
 
 }
 
+func handleGet(wr http.ResponseWriter, rr *http.Request) {
+	originalGetURL := rr.URL.String()
+	urlID := originalGetURL[1:]
+	originalURL := urlKeys[urlID]
+
+	// Debugging statements
+	fmt.Fprintf(wr, "originalGetURL: %s, urlID: %s, originalURL: %s\n", originalGetURL, urlID, originalURL)
+
+	// Check if originalURL is empty
+	if originalURL == "" {
+		http.Error(wr, "Original URL not found", http.StatusNotFound)
+		return
+	}
+
+	// Set headers and perform redirect
+	wr.Header().Add("Location", originalURL)
+	wr.WriteHeader(http.StatusTemporaryRedirect)
+
+	// Optional: You can include a message in the response body
+	fmt.Fprintf(wr, "Redirecting to: %s", originalURL)
+}
+
 func main() {
+
 	mux := http.NewServeMux()
 	mux.HandleFunc(`/`, func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
@@ -43,7 +68,7 @@ func main() {
 		case http.MethodGet:
 			handleGet(w, r)
 		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "Method not allowed", http.StatusBadRequest)
 		}
 	})
 
@@ -51,4 +76,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 }
